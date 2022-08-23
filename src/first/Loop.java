8882120d70c495b8ab14d61ffd4bc943085b881f
Ysmnsonmez <ysmnberk@gmail.com>
2022-08-23 11:11:35 -0500

    added the package in Loop.java

--- src/first/Loop.java
+++ src/first/Loop.java
@@ -1,7 +1,20 @@
 package first;
-public class Loop{
-  public static void main(String [] args){
-    for(int i = 0; i<5; i ++){
+public class Loop {
+  public static void main(String[] args) {
+    for (int i = 0; i < 5; i++) {
     }
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
